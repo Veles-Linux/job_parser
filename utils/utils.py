49e75f41b9d94207ba7a31012d4cec17cad9f7f6
Petrov Igor <piklaith@gmail.com>
2024-05-17 20:39:50 +0500

    Добавлена функция поиска вакансий по названию на HH.ru

---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+from utils.api_hh import HeadHunterAPI
+from utils.saveJSON import JSONSaver
+
+
+def user_choice_hh():
+    keyword = input('Напишите название профессии: \n')
+    hh_api = HeadHunterAPI()
+    print('Сколько вывести страниц? \n')
+    pages = int(input())
+    from_hh = hh_api.get_vacancies(keyword, pages)
+    print('Список вакансий с сайта "HeadHuter": \n')
+    for i in from_hh:
+        print(i)
+    print('Записать, отсортированные по зарплате данные в JSON файл? \n')
+    user_answer = input('Да\Нет \n').lower()
+    if user_answer not in ['да']:
+        print('Завершено')
+    else:
+        jsonfile_hh = JSONSaver()
+        jsonfile_hh.add_vacancies(from_hh)
+        jsonfile_hh.sort_vacancies_by_salary()
+        jsonfile_hh.file_writer()
+        return jsonfile_hh
