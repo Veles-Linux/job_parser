2982c8465b1270d07d587ae5a1c534427b4c1edd
Petrov Igor <piklaith@gmail.com>
2024-05-18 17:32:35 +0500

    Добавили возможность самостоятельно называть сохраняемый файл с вакансиями. По умолчанию имени нет.

--- utils/saveJSON.py
+++ utils/saveJSON.py
@@ -12,7 +12,7 @@ class JSONSaver(Vacancies, SaverJSON):
         """
         Запись файла
         """
-        with open('vacancies.json', 'w', encoding='utf-8') as file:
+        with open(input('Назовите файл: ') + '.json', 'w', encoding='utf-8') as file:
             json.dump(self.to_list_dict(), file, indent=4, ensure_ascii=False)
 
     def file_reader(self):
